f9da5f9a604b9381515dd71e96c7730dc1c73c94
Cristian Nambo Torres <ctorres@pacificbiosciences.com>
2024-04-08 16:53:34 -0700

    RR-6-improve-a-music-library-interface

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,18 +1,52 @@
 //These components will be making separate API calls from the app component to serve specific data about  our artist.
-import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import { useParams, useNavigate, useHistory } from 'react-router-dom'
 
-
-function AlbumView() {
-    const {id} = useParams()
+const AlbumView = (props) => {
+    const navigate = useNavigate
+    //const history = useHistory
+    const { id } = useParams()
     const [ albumData, setAlbumData ] = useState([])
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const API_URL = `http://localhost:4000/song/${id}`
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
 
-    return(
+    // const navButtons = () => {
+    //     return (
+    //         <div>
+    //             <button onClick={() => {history.push('/')}}>Home</button> | <button onClick={() => history.goBack()}>Back</button>
+    //         </div>
+    //     )
+    // }
+
+    const navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+
+    const allAlbums =   albumData.filter(entity => entity.kind === 'song')
+                        .map((album, i) => { return (<div key={i}>{album.trackName}</div>)})
+
+    return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here</p>
+            {albumData.length > 0 ? <h2>{albumData[0].collectionName}</h2> : <p>loading...</p>}
+            {navButtons()}
+            {allAlbums}
         </div>
     )
 }
+
 
 export default AlbumView
