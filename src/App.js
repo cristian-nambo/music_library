c072485cb0a34f19832bd994f7c9fbbd51fc4252
Cristian Nambo Torres <ctorres@pacificbiosciences.com>
2024-04-08 21:52:03 -0700

    with suspense-combine-music-library-features

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
-import { useEffect, useState } from 'react'
+import './App.css';
+import { useState, Suspense, useEffect } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import Spinner from './components/Spinner'
+import { createResource as fetchData } from './helper'
 
-function App(){
-    let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
+function App() {
+  let [searchTerm, setSearchTerm] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState(null)
 
-    const API_URL = 'https://itunes.apple.com/search?term='
+  useEffect(() => {
+    if (searchTerm) {
+      document.title=`${searchTerm} Music`
+      console.log(fetchData(searchTerm))
+      setData(fetchData(searchTerm))
+  }
+  }, [searchTerm])
 
-    useEffect(() => {
-        if(search) {
-      const fetchData = async () => {
-          document.title = `${search} Music`
-          const response = await fetch(API_URL + search)
-          const resData = await response.json()
-          if (resData.results.length > 0) {
-              setData(resData.results)
-          } else {
-              setMessage('Not Found')
-          }
-      }
-      fetchData()
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearchTerm(term)
+  }
+
+  const renderGallery = () => {
+    if(data){
+      return (
+        <Suspense fallback={<Spinner />}>
+          <Gallery data={data} />
+        </Suspense>
+      )
     }
-    }, [search])
+  }
 
-    const handleSearch = (e, term) => {
-        e.preventDefault()
-        setSearch(term)
-    }
-
-    return (
-        <div>
-            <SearchBar handleSearch = {handleSearch}/>
-            {message}
-            <Gallery data={data}/>
-        </div>
-    )
+  return (
+    <div className="App">
+      <SearchBar handleSearch={handleSearch} />
+      {message}
+      {renderGallery()}
+    </div>
+  );
 }
 
-export default App
+export default App;
